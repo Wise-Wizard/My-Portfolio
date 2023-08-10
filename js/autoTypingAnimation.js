2d9cf7139a6b8cecf0376d8ce79cb696f656b60a
Wise-Wizard <saransh.shankar@gmail.com>
2023-08-10 20:34:15 +0530

    Portfolio Project

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+  $(".about-info-inner").typed({
+    strings: [
+      "<span>Hello, I am Saransh Shankar.</span> I am a passionate developer with a diverse skill set. My expertise lies in the realm of Machine Learning, where I create innovative applications that harness the power of data. I am also well-versed in React technologies, crafting dynamic and user-friendly web solutions. Additionally, I excel in Flutter development, creating captivating cross-platform applications. I have a strong foundation in Full Stack Development, bolstered by impactful internships, including my recent role at Andwemet Services Pvt Ltd. Through these experiences, I've contributed to substantial projects, crafting functional and visually appealing websites. You can explore my portfolio on GitHub | Saransh Shankar to learn more about my work. Beyond coding, I immerse myself in reading technical blogs to stay at the forefront of advancements. I thrive on new experiences, meeting people, and excelling in fast-paced environments. My multidisciplinary skills and passion for growth make me a versatile asset ready to take on exciting challenges. Looking forward to new opportunities and collaborations! </span>",
+    ],
+    typeSpeed: 1,
+    contentType: "html",
+  });
+}, 500);
